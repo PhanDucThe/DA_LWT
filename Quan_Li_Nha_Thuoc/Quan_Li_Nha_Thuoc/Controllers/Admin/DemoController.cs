4698e7c176bd28dd53d0ca008555b86051e86d05
PhanDucThe <maytinh519@gmail.com>
2025-10-22 10:25:44 +0700

    Them

---
+++ Quan_Li_Nha_Thuoc/Quan_Li_Nha_Thuoc/Controllers/Admin/DemoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quan_Li_Nha_Thuoc.Controllers.Admin
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
